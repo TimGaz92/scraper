f15176f55af71d968e7ae767991b7f5c7bea86fc
TimGaz92 <tgaziano@optonline.net>
2017-08-06 12:58:18 -0400

    olympic route and db working

--- server.js
+++ server.js
@@ -19,14 +19,15 @@ db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-// Main route - make this a landing page 
+// Main route 
 app.get("/", function(req, res) {
   	res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-//this is the finder route 
+//this is the finder route for the archery sub-reddit
 app.get("/findred", function(req, res) {
-	res.send("scraping https://www.reddit.com/r/archery")
+  res.sendFile(path.join(__dirname, "public/find.html"));
+	// res.send("scraping https://www.reddit.com/r/archery")
 	request("https://www.reddit.com/r/archery", function(error, response, html) {
   		var $ = cheerio.load(html);
   		var results = [];
@@ -44,11 +45,11 @@ app.get("/findred", function(req, res) {
 	});
 });
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//route to find the world archery news page 
 app.get("/findworld", function(req, res) {
+  // res.sendFile(path.join(__dirname, "public/find.html"));
   res.send("scraping https://worldarchery.org/news")
   request("https://worldarchery.org/news", function(error, response, html) {
-    //https://worldarchery.org/news
       var $ = cheerio.load(html);
       var results = [];
       $("p.title").each(function(i, element) {
@@ -60,11 +61,33 @@ app.get("/findworld", function(req, res) {
         });
       });
     console.log(results);
-    db.scrapedData.insert(results)
+    db.scrapedDataWorld.insert(results)
+  });
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//extra route to find another site - don't knpo what yet
+app.get("/findolympicred", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/findo.html"));
+  // res.send("scraping https://www.reddit.com/r/archery")
+  request("https://www.reddit.com/r/olympicarchery/", function(error, response, html) {
+      var $ = cheerio.load(html);
+      var results = [];
+      $("p.title").each(function(i, element) {
+      var title = $(element).text();
+      var link = $(element).children().attr("href");
+        results.push({
+          title: title,
+          link: link
+        });
+      });
+    console.log(results);
+    console.log("test log----------------------------------------------------");
+    db.scrapedDataOlympicRed.insert(results)
   });
 });
 
-//route to display the results
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//route to display the results  - reddit archery
 app.get("/results", function(req, res){
 	  db.scrapedData.find({}, function(error, found) {
     if (error) {
@@ -74,7 +97,35 @@ app.get("/results", function(req, res){
       res.json(found);
     }
   });		
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//olympic archery reddit  results
+app.get("/resultso", function(req, res){
+    db.scrapedDataOlympicRed.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });   
 })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//world archery results route 
+app.get("/resultsworld", function(req, res){
+    db.scrapedDataWorld.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });   
+})
+
+
+
 
 
 // Listen on port 3000
